16c42d0a0becf9139c0554a0360c6597ee925003
Satyamraj7806 <0786satyamraj@gmail.com>
2026-03-04 23:33:47 +0530

    added different profiles for scanning

---
+++ catnet/core/profiles.py
@@ -0,0 +1,14 @@
+scan_profiles = {
+    "quick" :{
+        "name" : "Quick Scan",
+        "nmap_args" : ["-T4", "--top-ports", "100"]
+    },
+    "full" : {
+        "name" : "Full Scan",
+        "nmap_args" : ["-sS","-sV","-O", "-T4", "-p-"]
+    },
+    "stealth" : {
+        "name" : "Stealth Scan",
+        "nmap_args" : ["-T2", "-sS"]
+    }
+}
